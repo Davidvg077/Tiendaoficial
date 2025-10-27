0b01d0d518b98566db657ea9819b00dd55711055
Omar David Valderrama <odvalderrama16@ucatolica.edu.co>
2025-10-27 16:56:29 -0500

    Endpoint de crear categoria

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, HTTPException
+from models import Categoria, Producto
+from database import init_db
+import crud
+from Schemas import CategoriaUpdate, ProductoUpdate, CategoriaConProductos, ProductoResponse, ProductoListResponse, RestarStock
+
+app = FastAPI(title="API Tienda con SQLModel")
+
+# Crear la base de datos al iniciar
+@app.on_event("startup")
+def startup_event():
+    init_db()
+
+
+# Endpoints de categorias
+
+@app.post("/categorias/", response_model=Categoria)
+async def crear_categoria(categoria: Categoria):
+    return await crud.crear_categoria(categoria)
