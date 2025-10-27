f383e3a884e5521c80598bc533d174cb50dafae5
Omar David Valderrama <odvalderrama16@ucatolica.edu.co>
2025-10-26 19:24:31 -0500

    Creacion de schemas y añadimos Esquemas de categoria (CategoriaBase)

---
+++ schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field, conint, constr
+from typing import Optional, List
+
+# Esquemas de Categoria 
+
+class CategoriaBase(BaseModel):
+    nombre: constr(min_length=1, max_length=100)
+    descripcion: Optional[str] = None
+    activa: Optional[bool] = True
