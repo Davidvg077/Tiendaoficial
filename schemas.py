ab21c4ee00dd4d8d60da27488c41dd1382e4ff92
Omar David Valderrama <odvalderrama16@ucatolica.edu.co>
2025-10-26 19:29:36 -0500

    Añadimos CategoriaCreate y CategoriaUpdate

--- schemas.py
+++ schemas.py
@@ -7,3 +7,8 @@ class CategoriaBase(BaseModel):
     nombre: constr(min_length=1, max_length=100)
     descripcion: Optional[str] = None
     activa: Optional[bool] = True
+
+class CategoriaCreate(CategoriaBase):
+    """Esquema para crear una categoría"""
+    pass
+
